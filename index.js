9b9948b12877607ae0cceaa68b611653e19912c6
sleklere <santiagoleklere@gmail.com>
2022-12-25 22:51:59 -0300

    OOP implemented to possibly re-use the bot

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 ////////////////
-const fs = require("fs")
-const https = require("https")
-
 const {
-  delay,
-  tweet,
-  getOneTweet,
-  getTweets,
-  getMentionedTweets,
-  checkIfAlreadyReplied,
-  getMediaURLs,
-  replyWithMediaUrls,
+  // delay,
+  // tweet,
+  // getOneTweet,
+  // getTweets,
+  // getMentionedTweets,
+  // checkIfAlreadyReplied,
+  // getMediaURLs,
+  // replyWithMediaUrls,
+  Bot,
 } = require("./utils")
+
 require("dotenv").config({ path: __dirname + "/.env" })
+
 const { twitterClient, twitterBearer } = require("./twitterClient.js")
+
+const bot = new Bot(twitterClient.v2)
+
 const express = require("express")
 
 ////////////////
@@ -28,11 +31,11 @@ app.listen(8000, (req, res) => {
 // const clientSecret = "mWoLK7kR0PdMdRCoPtJOXOqT_XRUBWt75hhkoIqq5oug4vfJ6K";
 
 const init = async function () {
-  const tweets = (await getMentionedTweets()).data.data
+  const tweets = (await bot.getMentionedTweets()).data.data
 
   for (const tw of tweets) {
     // if replied matches a tw (in the same conversation) that is from the bot and has a 'in_reply_to_user_id' == tweet.author_id it will return true. this means the bot already replied to a tweet from that author tagging the bot.
-    const replied = await checkIfAlreadyReplied(
+    const replied = await bot.checkIfAlreadyReplied(
       tw.conversation_id,
       tw.author_id
     )
@@ -40,13 +43,13 @@ const init = async function () {
       const refTweets = tw.referenced_tweets
       // checks if the tw that tags the bot is a reply
       if (refTweets !== undefined && refTweets[0].type === "replied_to") {
-        const mediaURLs = await getMediaURLs(tw.referenced_tweets[0].id)
+        const mediaURLs = await bot.getMediaURLs(tw.referenced_tweets[0].id)
         // reply to the tweet where the bot was mentioned
-        replyWithMediaUrls(mediaURLs, tw.id)
-        await delay(10)
+        bot.replyWithMediaUrls(mediaURLs, tw.id)
+        await bot.delay(10)
       }
     }
   }
 }
 
-init()
+// init()
--- utils.js
+++ utils.js
@@ -1,122 +1,130 @@
 require("dotenv").config({ path: __dirname + "/.env" })
 const { twitterClient, twitterBearer } = require("./twitterClient.js")
 
-const delay = seconds =>
-  new Promise(resolve => setTimeout(resolve, seconds * 1000))
-
-const tweet = async tweetText => {
-  try {
-    await twitterClient.v2.tweet(tweetText)
-  } catch (e) {
-    console.log(e)
+class Bot {
+  constructor(client) {
+    this.twClientV2 = client
   }
-}
 
-const getOneTweet = async function (tweetId) {
-  const tweet = await twitterClient.v2.singleTweet(tweetId, {
-    expansions: "attachments.media_keys",
-    "tweet.fields": ["conversation_id"],
-  })
-  console.log(tweet)
-  return tweet
-}
-
-const getTweets = async function (query) {
-  console.log("==========")
-  console.log(`GET_TWEETS with query: ${query}`)
-  console.log("==========")
-  const response = await twitterClient.v2.search(query, {
-    expansions:
-      "author_id,attachments.media_keys,entities.mentions.username,in_reply_to_user_id",
-    "tweet.fields": ["conversation_id"],
-  })
-  return response
-}
-
-const getMentionedTweets = async function () {
-  const date = new Date("2023-01-01")
-  return await twitterClient.v2.userMentionTimeline("1523649543820267520", {
-    end_time: date.toISOString(),
-    max_results: 12,
-    expansions:
-      "author_id,attachments.media_keys,in_reply_to_user_id,referenced_tweets.id",
-    "media.fields": ["alt_text", "variants", "url"],
-    "tweet.fields": ["conversation_id"],
-  })
-}
-
-const checkIfAlreadyReplied = async function (conversation_id, authorId) {
-  const search = await getTweets(`conversation_id:${conversation_id}`)
-  const tweets = search.data.data
-  // console.log(tweets)
-  if (search.data.meta.result_count === 0) {
-    // console.log(search.data.meta)
-    console.log("No conversation")
-    return 3
+  delay(seconds) {
+    return new Promise(resolve => setTimeout(resolve, seconds * 1000))
   }
-  for (const tw of tweets) {
-    // check if the tw in the conversation is from the bot
-    if (tw.author_id === process.env.APP_ID) {
-      // check if it is a reply to the tw with the authorId from the arguments
-      if (tw.in_reply_to_user_id === authorId) {
-        // console.log(tw)
-        console.log("Already replied!")
-        return true
+
+  async tweet(tweetText) {
+    try {
+      await this.twClientV2.tweet(tweetText)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  async getOneTweet(tweetId) {
+    const tweet = await this.twClientV2.singleTweet(tweetId, {
+      expansions: "attachments.media_keys",
+      "tweet.fields": ["conversation_id"],
+    })
+    console.log(tweet)
+    return tweet
+  }
+
+  async getTweets(query) {
+    console.log("==========")
+    console.log(`GET_TWEETS with query: ${query}`)
+    console.log("==========")
+    const response = await this.twClientV2.search(query, {
+      expansions:
+        "author_id,attachments.media_keys,entities.mentions.username,in_reply_to_user_id",
+      "tweet.fields": ["conversation_id"],
+    })
+    return response
+  }
+
+  async getMentionedTweets() {
+    const date = new Date("2023-01-01")
+    return await this.twClientV2.userMentionTimeline("1523649543820267520", {
+      end_time: date.toISOString(),
+      max_results: 12,
+      expansions:
+        "author_id,attachments.media_keys,in_reply_to_user_id,referenced_tweets.id",
+      "media.fields": ["alt_text", "variants", "url"],
+      "tweet.fields": ["conversation_id"],
+    })
+  }
+
+  async checkIfAlreadyReplied(conversation_id, authorId) {
+    const search = await this.getTweets(`conversation_id:${conversation_id}`)
+    const tweets = search.data.data
+    // console.log(tweets)
+    if (search.data.meta.result_count === 0) {
+      // console.log(search.data.meta)
+      console.log("No conversation")
+      return 3
+    }
+    for (const tw of tweets) {
+      // check if the tw in the conversation is from the bot
+      if (tw.author_id === process.env.APP_ID) {
+        // check if it is a reply to the tw with the authorId from the arguments
+        if (tw.in_reply_to_user_id === authorId) {
+          // console.log(tw)
+          console.log("Already replied!")
+          return true
+        }
       }
     }
+    console.log("Didn't reply yet!")
+    return false
   }
-  console.log("Didn't reply yet!")
-  return false
-}
 
-const getMediaURLs = async function (tweetId) {
-  const tweet = await twitterClient.v2.singleTweet(tweetId, {
-    expansions: "attachments.media_keys",
-    "media.fields": ["variants", "url"],
-  })
-  const tweetMediaCopy = tweet.includes?.media.slice()
-  // console.log(tweet.data.attachments)
-  let mediaURLs = tweetMediaCopy?.map(mediaItem => {
-    switch (mediaItem.type) {
-      case "animated_gif":
-        return mediaItem.variants[0].url
-      case "photo":
-        return mediaItem.url
-      case "video":
-        // find the best quality video variant
-        return mediaItem.variants.reduce((prevVideo, curVideo) => {
-          if (curVideo.bit_rate == undefined) {
-            return prevVideo
-          }
-          if (curVideo.bit_rate > prevVideo.bit_rate) {
-            return curVideo.url
-          }
-        })
-    }
-  })
-  return mediaURLs
-}
-
-const replyWithMediaUrls = async function (mediaURLs, tweetId) {
-  try {
-    let replyText = `Enter this link/s to download your media:\n\n`
-    mediaURLs.forEach(url => {
-      replyText += `${url}\n`
+  async getMediaURLs(tweetId) {
+    const tweet = await this.twClientV2.singleTweet(tweetId, {
+      expansions: "attachments.media_keys",
+      "media.fields": ["variants", "url"],
     })
-    await twitterClient.v2.reply(replyText, tweetId)
-    console.log("Replied to tweet")
-  } catch (err) {
-    console.log(err.data.detail)
+    const tweetMediaCopy = tweet.includes?.media.slice()
+    // console.log(tweet.data.attachments)
+    let mediaURLs = tweetMediaCopy?.map(mediaItem => {
+      switch (mediaItem.type) {
+        case "animated_gif":
+          return mediaItem.variants[0].url
+        case "photo":
+          return mediaItem.url
+        case "video":
+          // find the best quality video variant
+          return mediaItem.variants.reduce((prevVideo, curVideo) => {
+            if (curVideo.bit_rate == undefined) {
+              return prevVideo
+            }
+            if (curVideo.bit_rate > prevVideo.bit_rate) {
+              return curVideo.url
+            }
+          })
+      }
+    })
+    return mediaURLs
+  }
+
+  async replyWithMediaUrls(mediaURLs, tweetId) {
+    try {
+      let replyText = `Enter this link/s to download your media:\n\n`
+      mediaURLs.forEach(url => {
+        replyText += `${url}\n`
+      })
+      await this.twClientV2.reply(replyText, tweetId)
+      console.log("Replied to tweet")
+    } catch (err) {
+      console.log(err.data.detail)
+    }
   }
 }
 
 module.exports = {
-  delay,
-  tweet,
-  getOneTweet,
-  getTweets,
-  getMentionedTweets,
-  checkIfAlreadyReplied,
-  getMediaURLs,
-  replyWithMediaUrls,
+  // delay,
+  // tweet,
+  // getOneTweet,
+  // getTweets,
+  // getMentionedTweets,
+  // checkIfAlreadyReplied,
+  // getMediaURLs,
+  // replyWithMediaUrls,
+  Bot,
 }
